101803919b45cf3f900236337b873144e34659c6
rupjit choudhury <rupjitchoudhury@gmail.com>
2023-11-07 18:13:33 +0530

    new bucket

--- sandbox1-account-customization/terraform/s3.tf
+++ sandbox1-account-customization/terraform/s3.tf
@@ -4,3 +4,12 @@ resource "aws_s3_bucket" "sandbox_bucket" {
   bucket = "aft-sandbox-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-poc-bucket-289234"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
