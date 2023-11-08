8c34b4d72671dfcded061332324839618dc03546
rupjit choudhury <rupjitchoudhury@gmail.com>
2023-11-08 14:10:00 +0530

    tag added, new bucket created

--- sandbox2-account-customization/terraform/s3.tf
+++ sandbox2-account-customization/terraform/s3.tf
@@ -3,4 +3,16 @@ data "aws_caller_identity" "current" {}
 resource "aws_s3_bucket" "sandbox_bucket2" {
   bucket = "aft-sandbox-23432342${data.aws_caller_identity.current.account_id}"
   acl    = "private"
+    tags = {
+    Name        = "My_bucket2"
+    Environment = "Dev"
+  }
+}
+resource "aws_s3_bucket" "sandbox_bucket3" {
+  bucket = "aft-sandbox-23423423${data.aws_caller_identity.current.account_id}"
+  acl    = "private"
+    tags = {
+    Name        = "My_bucket3"
+    Environment = "Dev"
+  }
 }
