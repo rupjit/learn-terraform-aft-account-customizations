a1aa535aaf024eb8a8781c238f7848695a47b148
rupjit choudhury <rupjitchoudhury@gmail.com>
2023-11-08 21:27:48 +0530

    new IAM

--- sandbox2-account-customization/terraform/s3.tf
+++ sandbox2-account-customization/terraform/s3.tf
@@ -16,3 +16,24 @@ resource "aws_s3_bucket" "sandbox_bucket3" {
     Environment = "Dev"
   }
 }
+
+resource "aws_iam_policy" "policy" {
+  name        = "test_policy"
+  path        = "/"
+  description = "My test policy"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
